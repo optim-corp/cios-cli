02a0e2016d04bbd4659ac1405573b7cc0ece1e58
kazuhiro.seida <kazuhiro.seida@optim.co.jp>
2021-04-22 14:03:05 +0900

    update #7 datasore download

--- cli/publishsubscribe/datastore.go
+++ cli/publishsubscribe/datastore.go
@@ -7,6 +7,8 @@ import (
 	"strings"
 	"time"
 
+	"github.com/optim-kazuhiro-seida/go-advance-type/wrapper"
+
 	. "github.com/optim-corp/cios-cli/cli"
 	"github.com/optim-corp/cios-cli/models"
 	"github.com/optim-corp/cios-cli/utils"
@@ -27,6 +29,7 @@ func GetDataStoreCommand() *cli.Command {
 			deleteDataStore(),
 			listDataStore(),
 			embezzleDateStore(),
+			saveDataStore(),
 		},
 	}
 }
@@ -94,84 +97,68 @@ func listDataStore() *cli.Command {
 		Aliases: models.ALIAS_LIST,
 		Flags: []cli.Flag{
 			&cli.StringFlag{Name: "channel_id", Aliases: []string{"c"}},
-			&cli.StringFlag{Name: "packer_format", Aliases: []string{"pf"}},
-			&cli.StringFlag{Name: "timestamp_range", Aliases: []string{"tr"}},
-			&cli.StringFlag{Name: "save_dir", Aliases: []string{"out"}},
+			&cli.StringFlag{Name: "packer_format", Aliases: []string{"pf"}, Value: "payload_only"},
+			&cli.StringFlag{Name: "timestamp_range", Aliases: []string{"tr"}, DefaultText: "Now Time", Value: ":" + convert.MustStr(time.Now().UnixNano())},
 			&cli.StringFlag{Name: "label", Aliases: []string{"lb"}},
 			&cli.BoolFlag{Name: "data", Aliases: []string{"d"}},
-			&cli.BoolFlag{Name: "save", Aliases: []string{"s"}},
 			&cli.Int64Flag{Name: "limit", Aliases: []string{"l"}, DefaultText: "30", Value: 30},
 			&cli.Int64Flag{Name: "offset", Aliases: []string{"o"}, DefaultText: "0", Value: 0},
 			&cli.StringFlag{Name: "resource_owner_id", Aliases: []string{"r", "ro"}},
-			&cli.StringFlag{Name: "change_channel_id", Aliases: []string{"chg-c"}},
-			&cli.StringFlag{Name: "data_replace", Aliases: []string{"replace", "rep"}, Usage: ""},
 		},
 		Action: func(c *cli.Context) error {
 			var (
 				channelID         = c.String("channel_id")
 				packerFormat      = c.String("packer_format")
-				outputDir         = c.String("save_dir")
 				limit             = c.Int64("limit")
 				offset            = c.Int64("offset")
-				timestampRange    = is(c.String("timestamp_range") != "").T(c.String("timestamp_range")).F(":" + convert.MustStr(time.Now().UnixNano())).Value.(string)
+				timestampRange    = c.String("timestamp_range")
 				resourceOwnerID   = c.String("resource_owner_id")
-				dataFlag          = c.Bool("data")
-				saveFlag          = c.Bool("save")
 				label             = c.String("label")
-				replace           = c.String("data_replace")
+				dataFlag          = c.Bool("data")
 				channelsMap, _, _ = Client.PubSub.GetChannelsMapByID(ciossdk.MakeGetChannelsOpts(), context.Background())
 			)
 
-			printObj := func(channel cios.Channel, limit int64) {
-				if dataFlag {
-					stageDSDir := datastoreDir + "/" + models.GetStage()
-					channelDir := datastoreDir + "/" + models.GetStage() + "/" + channelsMap[channel.Id].Name + "___" + channel.Id
-					data, err := Client.PubSub.GetStreamAll(channel.Id, ciossdk.MakeGetStreamOpts().Limit(limit).PackerFormat(packerFormat).TimestampRange(timestampRange).Label(label).Offset(offset), context.Background())
-					assert(err).Log()
-					fPrintf("\n|Channel ID|  : %s \n|Channel Name|: %s\n\n", channel.Id, channelsMap[channel.Id].Name)
-					if saveFlag {
-						path(datastoreDir).CreateDir()
-						path(stageDSDir).CreateDir()
-						path(channelDir).CreateDir()
-					}
+			printObject := func(channelId string) {
+				objects, _, err := Client.PubSub.GetObjectsAll(channelId, ciossdk.MakeGetObjectsOpts().
+					Limit(limit).
+					TimestampRange(timestampRange).
+					Label(label).
+					Offset(offset),
+					context.Background())
+				if len(objects) == 0 || resourceOwnerID != "" && channelsMap[channelId].ResourceOwnerId != resourceOwnerID {
+					return
+				}
+				fPrintf("\n|Channel ID|  : %s \n|Channel Name|: %s\n\n", channelId, channelsMap[channelId].Name)
+				fPrintln("\t|ID|\t\t|Timestamp|\t  |Mime Type|")
+				assert(err).
+					Log().
+					NoneErr(func() {
+						for _, obj := range objects {
+							fPrintf("%s %s %s\n", obj.Id, obj.Timestamp, obj.MimeType)
+						}
+					})
 
-					for count, val := range data {
-						if replace != "" {
-							splitInComma := strings.Split(replace, ",")
-							for _, _val := range splitInComma {
-								splitVal := strings.Split(_val, ":")
-								if len(splitVal) >= 2 {
-									val = strings.Replace(val, splitVal[0], splitVal[1], -1)
-								} else {
-									log.Warn("Replace Missing format string")
-								}
-							}
-						}
-						if saveFlag {
-							if outputDir != "" {
-								channelDir = outputDir
-							}
-							filePrefix := strings.Repeat("0", len(strconv.Itoa(len(data)))-len(strconv.Itoa(count)))
-							_path := channelDir + "/" + filePrefix + strconv.Itoa(count) + ".txt"
-							assert(path(_path).WriteFileAsString(val)).Log()
-							fPrintln(val)
-						}
-						fPrintln(val)
-					}
-				} else {
-					objects, _, err := Client.PubSub.GetObjectsAll(channel.Id, ciossdk.MakeGetObjectsOpts().Limit(limit).TimestampRange(timestampRange).Label(label).Offset(offset), context.Background())
-					if len(objects) == 0 || resourceOwnerID != "" && channelsMap[channel.Id].ResourceOwnerId != resourceOwnerID {
-						return
-					}
-					fPrintf("\n|Channel ID|  : %s \n|Channel Name|: %s\n\n", channel.Id, channelsMap[channel.Id].Name)
-					fPrintln("\t|ID|\t\t|Timestamp|\t  |Mime Type|")
-					assert(err).
-						Log().
-						NoneErr(func() {
-							for _, obj := range objects {
-								fPrintf("%s %s %s\n", obj.Id, obj.Timestamp, obj.MimeType)
-							}
-						})
+			}
+			printData := func(channelId string) []string {
+				data, err := Client.PubSub.GetStreamAll(channelId, ciossdk.MakeGetStreamOpts().
+					Limit(limit).Offset(offset).
+					PackerFormat(packerFormat).
+					TimestampRange(timestampRange).
+					Label(label),
+					context.Background())
+				assert(err).Log()
+				fPrintf("\n|Channel ID|  : %s \n|Channel Name|: %s\n\n", channelId, channelsMap[channelId].Name)
+				for _, val := range data {
+					fPrintln(val)
+				}
+				return data
+			}
+			printJob := func(channel cios.Channel, limit int64) {
+				switch {
+				case dataFlag:
+					printData(channel.Id)
+				default:
+					printObject(channel.Id)
 				}
 				assert(out.Flush()).Log()
 			}
@@ -181,18 +168,133 @@ func listDataStore() *cli.Command {
 					channel, _, err := Client.PubSub.GetChannel(channelID, nil, nil, context.Background())
 					assert(err).
 						Log().
-						NoneErr(func() { printObj(channel, limit) })
+						NoneErr(func() { printJob(channel, limit) })
 				} else {
 					channels, _, err := Client.PubSub.GetChannelsAll(ciossdk.MakeGetChannelsOpts().ResourceOwnerId(resourceOwnerID), context.Background())
 					assert(err).
 						Log().
 						NoneErr(func() {
 							for _, channel := range channels {
-								printObj(channel, limit)
+								printJob(channel, limit)
 							}
 						})
 				}
 			})
+			return nil
+		},
+	}
+}
+func saveDataStore() *cli.Command {
+	return &cli.Command{
+		Name:    "save",
+		Aliases: []string{"download", "get", "sv", "dl"},
+		Flags: []cli.Flag{
+			&cli.StringFlag{Name: "channel_id", Aliases: []string{"c"}},
+			&cli.StringFlag{Name: "packer_format", Aliases: []string{"pf"}, Value: "payload_only"},
+			&cli.StringFlag{Name: "timestamp_range", Aliases: []string{"tr"}, DefaultText: "Now Time", Value: ":" + convert.MustStr(time.Now().UnixNano())},
+			&cli.StringFlag{Name: "save_dir", Aliases: []string{"out"}},
+			&cli.StringFlag{Name: "label", Aliases: []string{"lb"}},
+			&cli.BoolFlag{Name: "data", Aliases: []string{"d"}},
+			&cli.BoolFlag{Name: "indent", Aliases: []string{"idt", "idnt", "i"}},
+			&cli.BoolFlag{Name: "collective", Aliases: []string{"compact", "coll", "collect"}},
+			&cli.Int64Flag{Name: "limit", Aliases: []string{"l"}, DefaultText: "30", Value: 30},
+			&cli.Int64Flag{Name: "offset", Aliases: []string{"o"}, DefaultText: "0", Value: 0},
+			&cli.StringFlag{Name: "resource_owner_id", Aliases: []string{"r", "ro"}},
+			&cli.StringFlag{Name: "replace_save_data_channel", Aliases: []string{"replace", "rep"}, Usage: "-replace <channel id>"},
+		},
+		Action: func(c *cli.Context) error {
+			var (
+				channelID         = c.String("channel_id")
+				packerFormat      = c.String("packer_format")
+				limit             = c.Int64("limit")
+				offset            = c.Int64("offset")
+				timestampRange    = c.String("timestamp_range")
+				resourceOwnerID   = c.String("resource_owner_id")
+				label             = c.String("label")
+				outputDir         = wrapper.AsString(c.String("save_dir"))
+				replaced          = wrapper.AsString(c.String("replace_save_data_channel"))
+				indent            = c.Bool("indent")
+				collective        = c.Bool("collective")
+				channelsMap, _, _ = Client.PubSub.GetChannelsMapByID(ciossdk.MakeGetChannelsOpts(), context.Background())
+				stageDSDir        = fmt.Sprintf("%s/%s", datastoreDir, models.GetStage())
+			)
+			replaceChannelId := func(data string) string {
+				var jsonFormat cios.PackerFormatJson
+				assert(convert.UnMarshalJson([]byte(data), &jsonFormat)).Log()
+				jsonFormat.Header.ChannelId = replaced.Str()
+				return convert.MustCompactJson(jsonFormat)
+			}
+			indentJson := func(data string) string {
+				var jsonFormat cios.PackerFormatJson
+				assert(convert.UnMarshalJson([]byte(data), &jsonFormat)).Log()
+				return convert.MustIndentJson(jsonFormat)
+			}
+			job := func(channel cios.Channel, limit int64) {
+				switch {
+				case replaced != "":
+					packerFormat = "json"
+					fallthrough
+				case outputDir == "":
+					outputDir = wrapper.String(fmt.Sprintf("%s/%s/%s___%s", datastoreDir, models.GetStage(), channelsMap[channel.Id].Name, channel.Id))
+					fallthrough
+				default:
+					data, err := Client.PubSub.GetStreamAll(channel.Id, ciossdk.MakeGetStreamOpts().
+						Limit(limit).Offset(offset).
+						PackerFormat(packerFormat).
+						TimestampRange(timestampRange).
+						Label(label),
+						context.Background())
+					assert(err).Log()
+					for idx, val := range data {
+						switch {
+						case replaced.IsPreset():
+							data[idx] = replaceChannelId(val)
+							fallthrough
+						case indent:
+							data[idx] = indentJson(val)
+						}
+					}
+					path(datastoreDir).CreateDir()
+					path(stageDSDir).CreateDir()
+					path(outputDir.Str()).CreateDir()
+					switch {
+					case collective:
+						fileName := fmt.Sprintf("%s/%s_%s.txt",
+							outputDir,
+							packerFormat,
+							strings.Replace(timestampRange, ":", "-", -1))
+						assert(path(fileName).WriteFileAsString("[\n"+strings.Join(data, ",")+"]\n")).
+							Log().
+							NoneErrPrintln("Completed ", fileName)
+					default:
+						for idx, val := range data {
+							allLength := len(str(len(data)))
+							currentLength := len(str(idx))
+							filePrefixZero := strings.Repeat("0", allLength-currentLength)
+							fileName := fmt.Sprintf("%s/%s%d.txt", outputDir, filePrefixZero, idx)
+							assert(path(fileName).WriteFileAsString(val)).
+								Log().
+								NoneErrPrintln("Completed ", fileName)
+						}
+					}
+				}
+			}
+
+			if channelID != "" {
+				channel, _, err := Client.PubSub.GetChannel(channelID, nil, nil, context.Background())
+				assert(err).
+					Log().
+					NoneErr(func() { job(channel, limit) })
+			} else {
+				channels, _, err := Client.PubSub.GetChannelsAll(ciossdk.MakeGetChannelsOpts().ResourceOwnerId(resourceOwnerID), context.Background())
+				assert(err).
+					Log().
+					NoneErr(func() {
+						for _, channel := range channels {
+							job(channel, limit)
+						}
+					})
+			}
 			return nil
 		},
 	}
