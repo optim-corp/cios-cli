a0cae6ec1c5e99d9d58c971ba446a5604f3de4b6
Shunsuke Naganuma <naganuma@optim.co.jp>
2022-09-11 19:21:30 +0900

    bugfix (#25)

--- cli/authorization/login.go
+++ cli/authorization/login.go
@@ -79,13 +79,17 @@ func login() {
 		},
 		{
 			Name:   "clientSecret",
-			Prompt: &survey.Password{Message: "Client Secret"},
+			Prompt: &survey.Password{Message: "Client Secret: "},
 		},
 	}, &answers)
 	answers.Stage = stage.Stage
 	basePath := setPath(answers.Stage)
-	port := answers.RedirectUri[16:21]
-	path := answers.RedirectUri[21:]
+	redirectUri, err := url.Parse(answers.RedirectUri)
+	if err != nil {
+		panic("Invlid Uri")
+	}
+	port := redirectUri.Port()
+	path := redirectUri.RequestURI()
 	http.HandleFunc(path, func(w http.ResponseWriter, r *http.Request) {
 		form := url.Values{}
 		form.Add("grant_type", "authorization_code")
